cd577bae67d79b19487ae055ee4c734d5528d91e
John Caruthers <john.d.caruthers@gmail.com>
2020-01-20 01:54:48 -0500

    Create credit.c
    work in progress still

---
+++ credit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/*int main(void) 
+{
+    int ccn = get_int("Enter your credit card number: "); //get use input
+    
+    checksum(ccn);
+    card_length(ccn);
+    
+    //check card length and starting digits
+    //print out resulting card
+}
+
+void checksum(int) 
+{
+    
+}
+*/
+
+
+
+
+int main(void) 
+{
+    int ccn = get_int("Credit Card: ");
+    printf("%i", ccn(0));
+}
