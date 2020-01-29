0da35211c2a82df0cb879ebfab90185a3092d607
John Caruthers <john.d.caruthers@gmail.com>
2020-01-29 06:33:31 -0500

    Update cmd_line.c

--- cmd_line.c
+++ cmd_line.c
@@ -3,15 +3,21 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 3) //can take up to 3 arguments in command line
+    if (argc > 3)
     {
-        printf("hello, %s %s\n!", argv[1], argv[2]);
+        printf("Can only take 3 arguments, try again!\n");
+        return 0;
+    }
+
+        else if (argc == 3) //can take up to 3 arguments in command line
+    {
+        printf("hello, %s %s!\n", argv[1], argv[2]);
 
     }
 
     else if (argc == 2)
     {
-        printf("hello, %s\n!", argv[1]);
+        printf("hello, %s!\n", argv[1]);
     }
 
     else
