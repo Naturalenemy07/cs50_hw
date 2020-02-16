0dce6193d3fe04c888ccc278e9a8597842c59eca
John Caruthers <john.d.caruthers@gmail.com>
2020-02-15 23:02:58 -0500

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -2,14 +2,53 @@
 #include<cs50.h>
 #include<string.h>
 
+int verify_length(string t);
+
 // run substitution with the 26 letter key
-// verify key meets all requirements
+int main(int argc, string argv[])
+{
+    string key = argv[1];
+
+    verify_unique_letters(key);
+    verify_length(key);
+
+    printf("You have the correct length key!\n");
+
+
+
+}
+
+int verify_unique_letters(string t)
+{
+
+}
+
+
+
+int verify_length(string t)
+{
+    int length = strlen(t);
+
+    if(length == 26)
+    {
+        return(0);
+    }
+
+    else
+    {
+        printf("The key must be 26 unique letters!\n");
+        return(1);
+        break();
+    }
+}
+// verify key meets all requirements:
 // input text
 // output encrypted text
 
+/*
 int main(void)
 {
-    string text = get_string("Enter text: "); // this is to test whether indivudual characters in a string can be printed
+    string text = get_string("Enter text: ");
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         printf("%c\n", text[i]);
@@ -18,4 +57,4 @@ int main(void)
 
 }
 
-// Now that individual strings can be accessed, I need to figure out how to take an input and set each letter in the alphabet to the key in successive order. 
+*/
