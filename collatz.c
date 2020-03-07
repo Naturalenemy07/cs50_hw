629030af6143221ee3b5461144bb671701e4cd0c
John Caruthers <john.d.caruthers@gmail.com>
2020-03-07 15:19:09 -0500

    Update collatz.c

--- collatz.c
+++ collatz.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int collatz(int n, int i);
+//this program calculates how many steps it takes to get back to 
+
+int main(void)
+{
+    int n = get_int("n: "); //gets input for what n should
+    int steps = collatz(n, i);
+    printf("Steps: %i\n", steps);
+}
 
 int collatz(int n, int i)
 {
@@ -9,13 +21,11 @@ int collatz(int n, int i)
 
     else if (n % 2 == 1)
     {
-        i++;
-        return collatz(3 * n + 1, i);
+        return 1 + collatz(3 * n + 1, i);
     }
 
     else
     {
-        i++;
-        return collatz(n / 2, i);
+        return 1 + collatz(n / 2, i);
     }
 }
