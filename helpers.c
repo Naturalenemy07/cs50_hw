d73efc9115043242ad8e7c0909a053c0396dcf39
John Caruthers <john.d.caruthers@gmail.com>
2020-04-28 21:16:26 -0400

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -58,7 +58,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    //set the new colors here; made three seperate arrays but maybe I can do a structure...
+    //set the new colors here = made three seperate arrays but maybe I can do a structure...
     int image_copy_R[height][width];
     int image_copy_G[height][width];
     int image_copy_B[height][width];
@@ -154,7 +154,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    //set the original image to the blurred colors
+    //not set the original image to the blurred colors
 
     for (int k = 0; k < height; k++)
     {
@@ -171,37 +171,134 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    int Gx_Kernel[3][3] = {-1, 0, 1},{-2, 0, 2},{-1, 0, 1};
-    int Gy_Kernel[3][3] = {-1, -2, -1},{0, 0, 0},{1, 2, 1};
-    
+    int Gx_Kernel[3][3] = {{-1, 0, 1},{-2, 0, 2},{-1, 0, 1}};
+    int Gy_Kernel[3][3] = {{-1, -2, -1},{0, 0, 0},{1, 2, 1}};
+
+    //can't figure out how to make a 3x3 matrix around the
+    int ULx = Gx_Kernel[1][1];
+    int Ux = Gx_Kernel[1][2];
+    int URx = Gx_Kernel[1][3];
+    int Lx = Gx_Kernel[2][1];
+    int Rx = Gx_Kernel[2][3];
+    int LLx = Gx_Kernel[3][1];
+    int Lx = Gx_Kernel[3][2];
+    int LRx = Gx_Kernel[3][3];
+
+    int ULy = Gy_Kernel[1][1];
+    int Uy = Gy_Kernel[1][2];
+    int URy = Gy_Kernel[1][3];
+    int Ly = Gy_Kernel[2][1];
+    int Ry = Gy_Kernel[2][3];
+    int LLy = Gy_Kernel[3][1];
+    int Ly = Gy_Kernel[3][2];
+    int LRy = Gy_Kernel[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%i ", Gx_Kernel[i][j])
+        }
+        printf("\n")
+    }
+
     int Gx_Red[8];
     int Gx_Green[8];
     int Gx_Blue[8];
-    
+
     int Gy_Red[8];
     int Gy_Green[8];
     int Gy_Blue[8];
-    
+
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //Corner pixels
+            if ((i == 0 && j == 0) || (i == height-1 && j == 0) || (i == 0 && j == width-1) || (i == height-1 && j == width-1))
+            {
+                //printf("Corner\n"); //test my corners (read 4)
+                if (i == 0 && j == 0)
+                {
+                     Gx_Red = image[i+1][j].rgbtRed * Ux + image[i+1][j+1].rgbtRed * URx + image[i][j+1].rgbtRed * Rx;
+                     Gx_Green = image[i+1][j].rgbtGreen * Ux + image[i+1][j+1].rgbtGreen * URx + image[i][j+1].rgbtGreen * Rx;
+                     Gx_Blue = image[i+1][j].rgbtBlue * Ux + image[i+1][j+1].rgbtBlue * URx + image[i][j+1].rgbtBlue * Rx;
+
+                     Gy_Red = image[i+1][j].rgbtRed * Uy + image[i+1][j+1].rgbtRed * URy + image[i][j+1].rgbtRed * Ry;
+                     Gy_Green = image[i+1][j].rgbtGreen * Uy + image[i+1][j+1].rgbtGreen * URy + image[i][j+1].rgbtGreen * Ry;
+                     Gy_Blue = image[i+1][j].rgbtBlue * Uy + image[i+1][j+1].rgbtBlue * URy + image[i][j+1].rgbtBlue * Ry;
+                }
+
+                else if(i == height-1 && j == 0)
+                {
+
+                }
+
+                else if (i == 0 && j == width-1)
+                {
+
+                }
+
+                else
+                {
+
+                }
+            }
+
+            //Edge pixels
+            else if((i == 0 || i == height-1 || j == 0 || j == width-1))
+            {
+                //printf("Edge "); //test edges
+                if (i == 0)
+                {
+
+                }
+
+                else if (i == height - 1)
+                {
+
+                }
+
+                else if (j == 0)
+                {
+
+                }
+
+                else
+                {
+
+                }
+            }
+
+            //Non-edge or Non-corner pixels (has 8 neighboring pixels)
+            else
+            {
+
+            }
+        }
+    }
+
     //Sobel Algorithm
     int n_red = sqrt(Gx_Red^2 + Gy_Red^2);
     int n_green = sqrt(Gx_Green^2 + Gy_Green^2);
     int n_blue = sqrt(Gx_Blue^2 + Gy_Blue^2);
-    
+
     //maximum value is 255...is there a better way to do this?
     if (n_red > 255)
     {
         n_red = 255;
     }
-    
+
     if (n_green > 255)
     {
         n_green = 255;
     }
-    
+
     if (n_blue > 255)
     {
         n_blue = 255;
     }
-    
+
     return;
 }
