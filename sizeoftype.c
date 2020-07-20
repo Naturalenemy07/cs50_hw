845e176c1a6076f5e0150a4fb2a03261a0c8b272
John Caruthers <john.d.caruthers@gmail.com>
2020-07-20 05:44:49 -0400

    Create sizeoftype.c

---
+++ sizeoftype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  printf("size of INT: %li bytes\n", sizeof(int));
+  printf("size of CHAR: %li bytes\n", sizeof(char));
+  printf("size of DOUBLE: %li bytes\n", sizeof(double));
+  printf("size of FLOAT: %li bytes\n", sizeof(float));
+}
